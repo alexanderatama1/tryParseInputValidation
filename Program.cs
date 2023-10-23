39cd8b7cdde91737e30e65ec69dca2e9b5e92683
Anthony Alexander Atama <100134398+alexanderatama1@users.noreply.github.com>
2023-10-23 18:13:10 +1100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿string input;
+
+int num;
+double val;
+
+do
+{
+    Console.Write("Enter a value: ");
+    input = Console.ReadLine();
+
+    if (double.TryParse(input, out val))
+    {
+        break;
+    }
+
+    Console.WriteLine("That was not a valid value!");
+} while (true);
+
+do
+{
+    Console.Write("Enter a positive interger: ");
+    input = Console.ReadLine();
+
+    if (int.TryParse(input, out num))
+    {
+        if (num > 0)
+        {
+            break;
+        }
+    }
+
+    Console.WriteLine("That was not a valid value!");
+} while (true);
